0059f5704ca17a70178371328ecfa620d51e971b
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2024-10-15 07:05:59 +0600

    Header Footer and Hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+
 const App = () => {
   return (
-    <>Welcome to tasker</>
+    <>
+      <Header />
+      <HeroSection/>
+      <Footer />
+    </>
   );
 };
 
